6cd725640f78b4c1f7dee182e3daf88effda0204
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2024-03-06 00:35:05 +0900

    [Gold IV] Title: 연구소, Time: 2552 ms, Memory: 34600 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,65 @@
+import sys,copy
+from collections import deque
+from itertools import combinations
+
+input = sys.stdin.readline
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(point,virus):
+    global max_area
+    while point:
+        now_arr = copy.deepcopy(arr)
+        block = point.popleft()
+        # 위치 순서를 좌표로 변환 후 해당 위치에 벽 세우기
+        for num in block:
+            now_arr[(num - 1) // m][(num - 1) % m] = 1
+        cnt = 0
+
+        que = deque()
+        for i,j in virus:
+            que.append(([i,j]))
+
+        while que:
+            x,y = que.popleft()
+
+            for i in range(4):
+                nx = x+dx[i]
+                ny = y+dy[i]
+                if 0<=nx<n and 0<=ny<m:
+                    if now_arr[nx][ny]==0:
+                        now_arr[nx][ny]=2
+                        que.append(([nx,ny]))
+        cnt=0
+        # 반복문 종료 시 안전 영역 수 파악
+        for i in now_arr:
+            cnt+= i.count(0)
+        max_area = max(max_area,cnt)
+
+
+n,m = map(int,input().split())
+
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+
+temp=[]
+# 0인 경우만 고려해야하기 때문에 0이 아닌 위치의 숫자를 제외한 조합을 만들기 위한 선별 작업
+for i in range(n):
+    for j in range(m):
+        if arr[i][j]==0:
+            temp.append(m*i+j+1)
+point = deque(combinations(temp, 3))
+
+virus = deque()
+
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 2:
+            virus.append([i,j])
+
+max_area = -float('inf')
+bfs(point,virus)
+
+print(max_area)
+

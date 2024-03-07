4a9c63b408307f6364e89c2512f9e4fe10fed67e
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2024-03-08 00:02:17 +0900

    [Silver I] Title: IOIOI, Time: 364 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,28 +3,18 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-s = input().rstrip()
+s = input().lstrip('O')
 
-# Pn은 무조건 I부터 시작한다. index()를 통해 가장 먼저 등장하는 I의 위치부터 시작
-start = s.index('I')
-limit = (n+1)+n
+ans,i,cnt=0,0,0
 
-pn = ''
-
-for i in range(limit):
-    if i%2==0:
-        pn+='I'
+while i <= m-2 :
+    if s[i:i+3] == 'IOI':
+        i+=2
+        cnt+=1
+        if cnt==n:
+            ans+=1
+            cnt-=1
     else:
-        pn+='O'
-
-cnt=0
-
-
-for i in range(start,m-limit+1):
-    # 첫 문자는 I로 시작해야하고 반드시 뒤에 O가 오기 때문에
-    # I뒤에 O가 아닌 경우는 확인하지 않는다.
-    if s[i]=='I' and s[i+1]=='O':
-        if s[i:i+limit] == pn:
-            cnt+=1
-
-print(cnt)
+        i+=1
+        cnt=0
+print(ans)

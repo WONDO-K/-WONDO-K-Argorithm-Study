f88b7eadc7330dab24ba6c91f7a4eaaaa2c519d4
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2024-09-01 18:03:32 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 636 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+a,b,c,d,e,f = map(int, input().split())
+
+for i in range(-999,1_000):
+    for j in range(-999,1_000):
+        if a*i + b*j == c:
+            if d*i + e*j == f:
+                print(i,j)
+

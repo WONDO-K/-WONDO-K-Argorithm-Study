9e3b0a2f9b82456deb1e9adb705cb64a8e36a208
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2024-11-13 23:24:04 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+
+        System.out.println(a + "??!");
+    }
+}

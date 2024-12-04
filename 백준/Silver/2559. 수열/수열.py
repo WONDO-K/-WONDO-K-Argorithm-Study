97bfc3616bb3efa5fc7af8079c2b56a1d0199bf9
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2024-12-05 01:09:14 +0900

    [Silver III] Title: 수열, Time: 84 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,11 +1,13 @@
 a,b = map(int, input().split())
+
 tmp_a =  list(map(int, input().split()))
 tmp_b = [0 for i in range(a+1)]
 ans = []
 for i in range(a):
     tmp_b[i+1] = tmp_b[i]+tmp_a[i]
 
-for i in range(b,a+1):
-    ans.append(tmp_b[i]-tmp_b[i-b])
+for i in range(a-b+1):
+
+    ans.append(tmp_b[i+b]-tmp_b[i])
 
 print(max(ans))

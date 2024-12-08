9a253ccad3a80c26e8e476142e2d96f29916594d
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2024-12-08 20:58:49 +0900

    [Gold V] Title: 개똥벌레, Time: 272 ms, Memory: 60844 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -1,24 +1,21 @@
 import sys
 input = sys.stdin.readline
-
-n, h = map(int,input().split())
+n,h = map(int,input().split())
 
 line = [0 for _ in range(h)]
 
-for t in range(n):
+for i in range(n):
     height = int(input())
-    if t%2 == 0:
-        line[0] += 1
-        line[height] -= 1 
-        
-    if t%2 == 1:
-        line[h-height] += 1
+
+    if i%2 == 0:
+        line[0]+=1
+        line[height] -=1
+    else:
+        line[h-height] +=1
 
 prefix = [0 for _ in range(h+1)]
 
 for i in range(h):
-    prefix[i+1] = prefix[i] + line[i]
-    
+    prefix[i+1] = prefix[i]+line[i]
 prefix = prefix[1:]
-
 print(min(prefix), prefix.count(min(prefix)))

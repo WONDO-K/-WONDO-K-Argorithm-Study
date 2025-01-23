689d593d220aafae9c1325a1a64754c9ba5ab931
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-01-23 23:37:24 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,36 +1,28 @@
-# 컴퓨터 수
+import sys
+
+input = sys.stdin.readline
+
+def dfs(x):
+    global cnt
+    visit[x]=1
+    cnt+=1
+
+    for nx in arr[x]:
+        if visit[nx] != 1:
+            dfs(nx)
+
+    
+
 n = int(input())
-# 노드 수
 m = int(input())
+visit = [0 for _ in range(n+1)]
+arr = [[] for i in range(n+1)]
 
-# 컴퓨터 연결 정보 배열
-arr=[[] for _ in range(n+1)]
-
-# 방문 기록
-d_visit = [False for _ in range(n+1)]
-b_visit = [False for _ in range(n+1)]
 for i in range(m):
     a,b = map(int,input().split())
     arr[a].append(b)
     arr[b].append(a)
-# 컴퓨터 수를 셀 cnt
-bfs_cnt=0
 
-def bfs(x):
-    global bfs_cnt
-
-    b_visit[x] = True
-    bfs_cnt+=1
-    queue = [x]
-
-    while queue:
-        x = queue.pop()
-        for nx in arr[x]:
-            if b_visit[nx]==True:
-                continue
-            else:
-                queue.append(nx)
-                b_visit[nx]=True
-                bfs_cnt+=1
-bfs(1) 
-print(bfs_cnt-1)
+cnt = 0
+dfs(1)
+print(cnt-1)

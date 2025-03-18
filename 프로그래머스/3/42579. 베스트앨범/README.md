e28b258dccad5e458d7ab89b69b2074f9f0f0be1
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-03-19 03:17:03 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 0.13 ms
+메모리: 9.18 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 18일 21:28:47
+2025년 03월 19일 03:16:59
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,25 +1,30 @@
 def solution(genre_array, play_array):
 
-    dic = dict()
-    result=[]
+    dic1 = dict() # 장르별 총 재생 수
+    dic2 = dict() # 장르별 몇 번 인덱스에 몇 번의 재생 수를 가지고 있는가?
+    result = []
     
-    for i in genre_array:
-        dic[i] = [0,[]]
+    # 장르별 총 재생 수
+    for i in range(len(genre_array)): # 딕셔너리는 기본적으로 키를 기준으로 검색하도록 설계됨-> dic1.keys()는 딕셔너리의 키 목록을 반환하는 불필요한 연산을 하게 된다.
+        genre = genre_array[i]
+        play = play_array[i]
+        if genre not in dic1: # 키가 없을 때
+            dic1[genre] = play
+            dic2[genre] = [[i,play]]
+        else: # 키가 있을 때
+            dic1[genre] += play
+            dic2[genre].append([i, play])
 
-    for i in range(len(genre_array)):
-        dic[genre_array[i]][0] += play_array[i]
-        dic[genre_array[i]][1].append([i,play_array[i]])
-        
-    temp = sorted(dic.items(),key=lambda x:-x[1][0])
-    
-    for genre in temp:
-        genre[1][1].sort(key=lambda x:(-x[1],x[0]))
 
-    for genre in temp:
-        if len(genre[1][1]) >= 2:
-            result.append(genre[1][1][0][0])
-            result.append(genre[1][1][1][0])
-        else:
-            result.append(genre[1][1][0][0])
-
+    # 장르별로 가장 재생 수가 많은 장르 중, 재생 수가 많은 순서대로
+    arr1 = sorted(dic1.items(),key=lambda x:x[1],reverse=True)
+    # 장르 내에서 가장 많은 재생 수 순서로 2곡을 넣는다.
+    for genre, total in arr1:
+        arr2 = sorted(dic2[genre],key=lambda x:-x[1])
+        cnt=0
+        for idx,play in arr2:
+            if cnt>=2:
+                break
+            result.append(idx)
+            cnt+=1
     return result

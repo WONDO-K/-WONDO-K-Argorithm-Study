4b4cfb6864ec05a842a1ce2e81fc800f521c10e0
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-03-18 21:19:04 +0900

    [level 3] Title: 베스트앨범, Time: 0.12 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,26 @@
+def solution(genre_array, play_array):
+
+    temp = set(genre_array)
+    dic = dict()
+    result=[]
+    
+    for i in temp:
+        dic[i] = [0,[]]
+
+    for i in range(len(genre_array)):
+        dic[genre_array[i]][0] += play_array[i]
+        dic[genre_array[i]][1].append([i,play_array[i]])
+        
+    temp = sorted(dic.items(),key=lambda x:-x[1][0])
+    
+    for genre in temp:
+        genre[1][1].sort(key=lambda x:(-x[1],x[0]))
+
+    for genre in temp:
+        if len(genre[1][1]) >= 2:
+            result.append(genre[1][1][0][0])
+            result.append(genre[1][1][1][0])
+        else:
+            result.append(genre[1][1][0][0])
+
+    return result

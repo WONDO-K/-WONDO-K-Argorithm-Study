2359f4a17a96331c610d83a22b5aec4f53a4a0dd
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-03-26 21:47:51 +0900

    [Silver I] Title: 극장 좌석, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.py
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -2,37 +2,31 @@ import sys
 input = sys.stdin.readline
 
 
-def get_all_ways_of_theater_seat(total_count, fixed_seat_array):
-    if len(fixed_seat_array) == total_count:
-        return 1
-    temp = []
-    arr = list(range(1,total_count+1))
+def solve(n, arr):
 
-    start = 0
-    for i in fixed_seat_array:
-        temp.append(arr[start:i-1])
-        start = i
-    temp.append(arr[start:])
+    dp = [0] * 41
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 2
 
+    for i in range(3, 41):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    prev = 0
     ans = 1
-    for i in temp:
-        if len(i) != 0:
-            ans = ans*dp[len(i)]
+
+    for limit in arr: # vip 꺼냄
+        ans *= dp[limit-1-prev] # limit(idx상 -1)에서 이전 위치를 뺀 값 = 해당 위치 까지의 갯수
+        prev = limit # 어디까지 갯수를 카운트 했는지 저장
+
+    ans *= dp[n-prev] # 마지막 vip 좌석 다음부터 끝 좌석까지
 
     return ans
 
-total_count = int(input())
-
-fixed_seat_array = []
+n = int(input())
+arr = []
 
 for _ in range(int(input())):
-    fixed_seat_array.append(int(input()))
+    arr.append(int(input()))
 
-dp = [0]*41
-dp[1]=1
-dp[2]=2
-
-for i in range(3,41):
-    dp[i] = dp[i-1]+dp[i-2]
-
-print(get_all_ways_of_theater_seat(total_count,fixed_seat_array))
+print(solve(n,arr))

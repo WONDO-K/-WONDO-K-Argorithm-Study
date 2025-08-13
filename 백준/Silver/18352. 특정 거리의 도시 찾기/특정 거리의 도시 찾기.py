949ec48f3c04810514709db407105b5567a12bf7
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-08-13 16:33:33 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 3372 ms, Memory: 182352 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,42 +1,42 @@
-import sys
-import math
+import sys, math
 import heapq
-
 input = sys.stdin.readline
 INF = math.inf
 
-def sol(start):
-    q = []
-    move = 0
-    heapq.heappush(q,(0,start))
-    cnt[start] = 0
 
-    while q:
-        move,now = heapq.heappop(q)
-        if move > cnt[now]:
+def dijkstra(start):
+    que = []
+    heapq.heappush(que,(0,start))
+    dist[start] = 0
+
+    while que:
+        cnt, now = heapq.heappop(que)
+
+        if cnt > dist[now]: # 현재 뽑은 이동횟수가 저장된 이동횟수보다 크면 해당 경로 추가x(삭제)
             continue
-        for next in arr[now]:
-            new_move = next[1]+move
-            if move < cnt[next[0]]:
-                cnt[next[0]] = new_move
-                heapq.heappush(q,(new_move,next[0]))
 
+        for nxt in maps[now]:
+            nxt_cnt = nxt[1]+cnt # 1번 인덱스에 가중치 담겨있음 꺼내서 지금까지의 이동거리 cnt와 더함 -> 다음 노드까지 예상 이동거리
 
+            if cnt < dist[nxt[0]]: # 현재 cnt가 다음 노드까지 최소거리보다 작다면 교체
+                dist[nxt[0]] = nxt_cnt
+                heapq.heappush(que,(nxt_cnt,nxt[0]))
 
 
 n,m,k,x = map(int,input().split())
-cnt = [INF]*(n+1)
-arr = [[] for _ in range(n+1)]
-result=[]
+maps = [[] for i in range(n+1)]
+dist = [INF] * (n+1)
 
+for _ in range(m):
+    s,e = map(int,input().split())
+    # 종착지와 가중치(거리 - 이 문제에서 모두 거리는 1로 동일함)
+    maps[s].append((e, 1))
+dijkstra(x)
 
-for i in range(m):
-    start,end = map(int,input().split())
-    arr[start].append((end,1))
-sol(x)
+result = []
 
-for i in range(1, n+1):
-    if cnt[i] == k: 
+for i in range(1,n+1):
+    if dist[i] == k:
         result.append(i)
 
 if len(result)==0:

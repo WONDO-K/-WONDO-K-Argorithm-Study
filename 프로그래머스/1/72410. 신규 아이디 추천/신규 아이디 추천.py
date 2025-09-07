ea97c79c0977871654e5e283ee495b24fdec6d8c
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-09-08 02:05:55 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,3 +1,14 @@
+# 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+# 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+# 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+# 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+# 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+# 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+#      만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+# 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+
+# a = 32, A = 65
+
 
 def solution(new_id):
     # 1단계 모든 대문자 -> 소문자로
@@ -10,24 +21,16 @@ def solution(new_id):
             temp += ch
     after_id = temp
 
-    temp = ''
-    prev = ''
-    
-    # 3단계
-    for text in after_id:
-        #print(f'text: {text}, prev : {prev}')
-        if text =='.' and prev == '.': # 다음 문자가 .이 아니게 되면 문자열 조작을 안함
-            continue
-        temp += text
-        prev = text
-    after_id = temp
+    while '..' in after_id: # ...이든 ....이든 .....이든 그 안에 ..이 포함되어 있기 때문에 ..이 없을 때까지 .으로 치환
+        after_id = after_id.replace('..','.')
+
 
     # 4단계
     after_id = after_id.strip('.')
 
     # 5단계
-    if len(after_id)==0:
-        after_id += 'a'
+    if after_id == '':
+        after_id = 'a'
 
     # 6단계
     if len(after_id) >= 16:
@@ -36,7 +39,13 @@ def solution(new_id):
             after_id = after_id[:-1]
 
     # 7단계
-    if len(after_id) <= 2:
-        for i in range(3-len(after_id)):
-            after_id+=after_id[-1]
+    while len(after_id)<3:
+        after_id += after_id[-1]
     return after_id
+
+id_list = ["...!@BaT#*..y.abcdefghijklm", "z-+.^.", "=.=", "123_.def", "abcdefghijklmn.p", "BBBBBBBB"]
+
+for new_id in id_list:
+    print(solution(new_id))
+
+

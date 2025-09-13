58c1bae6258f89358520d9adb9e058e539186d15
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-09-14 00:06:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.71 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,4 @@
+import collections
 def solution(participant, completion):
-
-    hash_map = {}
-
-    for name in completion:
-        if name not in hash_map:
-            hash_map[name] = 1
-        else:
-            hash_map[name] += 1
-
-    for name in participant:
-        if name not in hash_map:
-            return name
-        if hash_map[name] == 0:
-            return name
-        hash_map[name] -=1
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer)[0]

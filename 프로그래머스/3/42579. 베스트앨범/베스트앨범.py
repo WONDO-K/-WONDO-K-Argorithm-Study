caee47bf935145e5c2f0482a8d93b8a3dfa7ba8f
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-09-16 01:31:38 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,30 +1,27 @@
-def solution(genre_array, play_array):
+def solution(genres, plays):
+    answer = []
+    dict1 = {} # 장르 별 총 재생 횟수
+    dict2 = {}
+    for i in range(len(genres)):
+        genre = genres[i]
+        play = plays[i]
+        
+        if genre not in dict1:
+            dict1[genre] = play
+            dict2[genre] = [[i,play]]
+        else:
+            dict1[genre] += play
+            dict2[genre].append([i,play])
 
-    dic1 = dict() # 장르별 총 재생 수
-    dic2 = dict() # 장르별 몇 번 인덱스에 몇 번의 재생 수를 가지고 있는가?
-    result = []
-    
-    # 장르별 총 재생 수
-    for i in range(len(genre_array)): # 딕셔너리는 기본적으로 키를 기준으로 검색하도록 설계됨-> dic1.keys()는 딕셔너리의 키 목록을 반환하는 불필요한 연산을 하게 된다.
-        genre = genre_array[i]
-        play = play_array[i]
-        if genre not in dic1: # 키가 없을 때
-            dic1[genre] = play
-            dic2[genre] = [[i,play]]
-        else: # 키가 있을 때
-            dic1[genre] += play
-            dic2[genre].append([i, play])
+    arr1 = sorted(dict1.items(), key = lambda x:x[1], reverse=True)
 
-
-    # 장르별로 가장 재생 수가 많은 장르 중, 재생 수가 많은 순서대로
-    arr1 = sorted(dic1.items(),key=lambda x:x[1],reverse=True)
-    # 장르 내에서 가장 많은 재생 수 순서로 2곡을 넣는다.
     for genre, total in arr1:
-        arr2 = sorted(dic2[genre],key=lambda x:-x[1])
-        cnt=0
-        for idx,play in arr2:
-            if cnt>=2:
+        arr2 = sorted(dict2[genre], key=lambda x: -x[1])
+        cnt = 0
+        for idx, play in arr2:
+            if cnt >= 2:
                 break
-            result.append(idx)
-            cnt+=1
-    return result
+            answer.append(idx)
+            cnt += 1
+
+    return answer

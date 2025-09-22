b85d7b578eade20a5a503ac10f71f2d60abb7a62
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-09-22 17:41:52 +0900

    [level 1] Title: 달리기 경주, Time: 1076.70 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -8,10 +8,12 @@ def solution(players, callings):
     for call in callings:
         idx = hash_map[call]
 
+        # 딕셔너리의 idx 값 바꾸기 -> 이 작업이 먼저와야 하는 이유 : players의 위치를 바꾼후에는 player[idx-1]값이 변질되어 버림
+        # idx = 현재 호명한 추월 선수의 위치 -> 추월당한 선수의 위치로 바뀜
+        hash_map[call] -=1 # 추월한 선수
+        hash_map[players[idx-1]] +=1 # 추월당한 선수
+
         players[idx],players[idx-1] = players[idx-1],players[idx] # players의 위치 바꾸기
 
-        # 딕셔너리의 idx 값 바꾸기
-        hash_map[players[idx]] = idx
-        hash_map[players[idx-1]] = idx-1 # 추월한 선수의 위치가 추월당한 선수의 값으로 변경되었기 때문에
 
     return players

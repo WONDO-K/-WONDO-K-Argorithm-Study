453a9dfcfe652ed25b01218c9663fb86c2ba3edb
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2025-12-01 16:34:51 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    que = deque([])
+    for work, speed in zip(progresses,speeds):
+        day = (100 - work + speed -1)//speed
+        que.append(day)
+        
+    prev = que.popleft()
+    cnt = 1
+    while que:
+        day = que.popleft()
+        if day > prev:
+            answer.append(cnt)
+            prev = day
+            cnt=1
+        else:
+            cnt+=1
+    answer.append(cnt)
+    return answer
+
+
+
